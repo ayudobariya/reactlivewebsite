da1be733959f719607fe81b5c1156372d123264c
ayudobariya <142329112+ayudobariya@users.noreply.github.com>
2023-10-13 17:45:47 +0530

    my first commit reactwebsite

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer =()=>{
+	return(
+		<>
+			<footer className="w-100 bg-light text-center">
+				<p>© 2023 MyApp. All Rights Reserved | Terms and Condition </p>
+			</footer>
+		</>
+	)
+};
+
+export default Footer;
---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+import web from "../src/image/sr.jpg";
+import app from "../src/image/sr2.jpeg";
+import software from "../src/image/sr3.jpeg";
+import digital from "../src/image/sr4.jpeg";
+import android from "../src/image/sr5.jpeg";
+import marketing from "../src/image/sr6.jpg";
+
+const Sdata = [
+	{
+		imgsrc: web,
+		title: "web development",
+	},
+	{
+		imgsrc: app,
+		title: "App development",
+	},
+	{
+		imgsrc: software,
+		title: "Software development",
+	},
+	{
+		imgsrc: digital,
+		title: "Digital marketing",
+	},
+	{
+		imgsrc: android,
+		title: "Android development",
+	},
+	{
+		imgsrc: marketing,
+		title: "Marketing",
+	}
+];
+
+export default Sdata;
